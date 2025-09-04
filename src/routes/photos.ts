acfba2daa0691663efcd35800d7948923ec162e8
Gloireski <belemgloirebekoutou8@gmail.com>
2025-09-04 15:14:06 +0200

    fix add photo

--- src/routes/photos.ts
+++ src/routes/photos.ts
@@ -2,10 +2,11 @@ import { Router } from "express";
 import { asyncHandler } from "../misc/errors";
 import { authMiddleware, AuthRequest } from "../misc/auth";
 import { createPhotoController, listPhotosController, getPhotoController, deletePhotoController } from "../controllers/photosController";
+import { upload } from "../middleware/upload";
 
 const router = Router();
 
-router.post("/", authMiddleware, asyncHandler(createPhotoController));
+router.post("/", authMiddleware, upload.single("image"), asyncHandler(createPhotoController));
 
 router.get("/", authMiddleware, asyncHandler(listPhotosController));
 
